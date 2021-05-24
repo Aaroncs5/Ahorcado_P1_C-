98d74259709f150600efc6dcc77e49a0fde0d14b
David Barillas <aaron2500zone@gmail.com>
2021-05-24 11:47:57 -0600

    Inicio del ahorcado c#

---
+++ ahorcado/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace ahorcado
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.FLP1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.FLP2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.LBLMensaje = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Perpetua Titling MT", 36F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(367, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 58);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "¡AHORCADO!";
+            // 
+            // FLP1
+            // 
+            this.FLP1.Location = new System.Drawing.Point(109, 260);
+            this.FLP1.Name = "FLP1";
+            this.FLP1.Size = new System.Drawing.Size(494, 326);
+            this.FLP1.TabIndex = 1;
+            // 
+            // FLP2
+            // 
+            this.FLP2.Location = new System.Drawing.Point(35, 592);
+            this.FLP2.Name = "FLP2";
+            this.FLP2.Size = new System.Drawing.Size(632, 100);
+            this.FLP2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(228, 218);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(277, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ADIVINA LA PALABRA";
+            // 
+            // LBLMensaje
+            // 
+            this.LBLMensaje.AutoSize = true;
+            this.LBLMensaje.Font = new System.Drawing.Font("Perpetua Titling MT", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.LBLMensaje.ForeColor = System.Drawing.Color.Red;
+            this.LBLMensaje.Location = new System.Drawing.Point(908, 208);
+            this.LBLMensaje.Name = "LBLMensaje";
+            this.LBLMensaje.Size = new System.Drawing.Size(117, 26);
+            this.LBLMensaje.TabIndex = 5;
+            this.LBLMensaje.Text = "PERDISTE";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Century Gothic", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(480, 108);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(133, 54);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "INICIAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(756, 262);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(432, 415);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // fileSystemWatcher1
+            // 
+            this.fileSystemWatcher1.EnableRaisingEvents = true;
+            this.fileSystemWatcher1.SynchronizingObject = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(1231, 721);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.LBLMensaje);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.FLP2);
+            this.Controls.Add(this.FLP1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel FLP1;
+        private System.Windows.Forms.FlowLayoutPanel FLP2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label LBLMensaje;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.IO.FileSystemWatcher fileSystemWatcher1;
+    }
+}
+
---
+++ ahorcado/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ahorcado
+{
+    public partial class Form1 : Form
+    {
+        char[] PalabrasAdivinadas; int Oportunidades;
+        char[] PalabrasSeleccionada;
+        char[] Alfabeto;
+        string[] PALABRA;
+
+        public void IniciarJuego()
+        {
+            
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            FLP1.Controls.Clear();
+            FLP1.Enabled = true;
+            pictureBox1.Image = null;
+            LBLMensaje.Visible = false;
+            Oportunidades = 0;
+            PALABRA = new string[] { "Hola", "Universidad", "Counter", "Telefono", "Celular", "Mouse", "Pachon", "Mesa", "cuaderno", "Tiktok", "wismichu", "Computadora", "Programación", "Calculadora", "Hoja", "Cocodrilo" };
+            Alfabeto = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ".ToCharArray();
+
+            Random nR = new Random();
+            int IndiceDeLaPalabraSeleccionada = nR.Next(0, PALABRA.Length);
+            PalabrasSeleccionada = PALABRA[IndiceDeLaPalabraSeleccionada].ToUpper().ToCharArray();
+            PalabrasAdivinadas = PalabrasSeleccionada;
+
+            foreach (Char LetrasAlfabeto in Alfabeto)
+            {
+                Button BTNLetra = new Button();
+                BTNLetra.Tag = "";
+                BTNLetra.Text = LetrasAlfabeto.ToString();
+                BTNLetra.Width = 60;
+                BTNLetra.Height = 40;
+                BTNLetra.ForeColor = Color.White;
+                BTNLetra.Font = new Font(BTNLetra.Font.Name, 15, FontStyle.Italic);
+                BTNLetra.BackColor = Color.Black;
+                BTNLetra.Name = LetrasAlfabeto.ToString();
+                FLP1.Controls.Add(BTNLetra);
+            }
+            FLP2.Controls.Clear();
+
+            for(int INValorDeLetra =0;INValorDeLetra < PalabrasSeleccionada.Length; INValorDeLetra++)
+            {
+                TextBox ColocaRP4 = new TextBox();
+                ColocaRP4.Tag = PalabrasSeleccionada[INValorDeLetra].ToString();
+                ColocaRP4.Width = 50;
+                ColocaRP4.Height = 80;
+                ColocaRP4.ForeColor = Color.SkyBlue;
+                ColocaRP4.Text = "-";
+                ColocaRP4.Font = new Font(ColocaRP4.Font.Name, 32, FontStyle.Italic);
+                ColocaRP4.BackColor = Color.Beige;
+                ColocaRP4.Name = "Adivinando" + INValorDeLetra.ToString();
+                FLP2.Controls.Add(ColocaRP4);
+
+            }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+    }
+
+}
